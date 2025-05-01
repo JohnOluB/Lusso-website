a62b791f212701989f80cc7091affa324c782f1d
JohnOluB <adebayodeolu90@gmail.com>
2025-05-02 00:56:01 +0100

    add: hero page

---
+++ lusso/src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css";
+const Hero = () => {
+  return (
+    <div className="container">
+      <span className="header">LussoVentures</span>
+      <p>Redefining Global Luxury Real Estate</p>
+<p>Lusso Ventures is a private investment fund building iconic living experiences in the world's most coveted cities.</p>
+<button>Request Investor Access</button>
+    </div>
+  );
+};
+
+export default Hero;
--- lusso/src/main.jsx
+++ lusso/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import "./index.css";
+import Hero from "./Components/Hero/Hero.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Hero />
+  </StrictMode>
+);
