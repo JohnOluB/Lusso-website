5aaddf9111b6895ff41349b5956bb5459407239f
JohnOluB <adebayodeolu90@gmail.com>
2025-05-05 10:10:16 +0100

    fix: container styles

--- lusso/src/Components/Hero/Hero.jsx
+++ lusso/src/Components/Hero/Hero.jsx
@@ -3,13 +3,16 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="container">
-      <span className="header">LussoVentures</span>
-      <p>Redefining Global Luxury Real Estate</p>
-      <p>
-        Lusso Ventures is a private investment fund building iconic living
-        experiences in the <p>world's most coveted cities.</p>
-      </p>
-      <button>Request Investor Access</button>
+      <span className="header">LussoVentures</span>{" "}
+      <p className="SubHeader">Redefining Global Luxury Real Estate</p>
+      <div className="container-one">
+        <p>
+          Lusso Ventures is a private investment fund building iconic living
+          experiences in the
+        </p>{" "}
+        <span>world's most coveted cities.</span>
+      </div>
+      <button className="btn">Request Investor Access</button>
     </div>
   );
 };
